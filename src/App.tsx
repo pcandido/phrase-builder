12303e036262c6dee5c1569ccaa74f7416f18bb4
Paulo Cândido <paulo.candido@g.globo>
2021-11-02 14:43:25 -0300

    feat: clear App and add verb-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { VerbInput } from './components/verb-input/VerbInput'
+import './App.css'
 
-function App() {
+export const App = () => {
+
+  const [verb, setVerb] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VerbInput value={verb} onChange={setVerb} />
+      <h1>{verb}</h1>
     </div>
-  );
+  )
 }
-
-export default App;
