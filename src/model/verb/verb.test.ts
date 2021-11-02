7c9a3c69d6b466df4e12b862b1cc4523c4f6a7ba
Paulo Cândido <paulo.candido@g.globo>
2021-11-01 21:26:15 -0300

    feat: add method getPresentSimple to verb

--- src/model/verb/verb.test.ts
+++ src/model/verb/verb.test.ts
@@ -1,3 +1,4 @@
+import { SHE, YOU } from '..'
 import { Verb } from './verb'
 
 describe('Verb', () => {
@@ -5,6 +6,48 @@ describe('Verb', () => {
   it('should return value as infinitive form', () => {
     const verb = new Verb('do')
     expect(verb.getInfinitive()).toBe('do')
+  })
+
+  describe('getPresentSimple', () => {
+
+    it('should use infinitive form when subject is not thirdPerson', () => {
+      const subject = YOU
+      const verb = new Verb('do')
+      expect(verb.getPresentSimple(subject)).toBe('do')
+    })
+
+    describe('subject is third-person', () => {
+      const subject = SHE
+
+      it('should add s to the end of infinitive form, for most of verbs', () => {
+        const verb = new Verb('read')
+        expect(verb.getPresentSimple(subject)).toBe('reads')
+      })
+
+      it.each([
+        ['o', 'do', 'does'],
+        ['s', 'confess', 'confesses'],
+        ['z', 'buzz', 'buzzes'],
+        ['ch', 'catch', 'catches'],
+        ['sh', 'finish', 'finishes'],
+        ['x', 'fix', 'fixes'],
+      ])('should add es to the end of verbs which ends with "%s", e.g. %s -> %s', (end, infinitiveForm, expectedForm) => {
+        const verb = new Verb(infinitiveForm)
+        expect(verb.getPresentSimple(subject)).toBe(expectedForm)
+      })
+
+      it('should replace y with ies on verbs which ends with "y", e.g. fly -> flies', () => {
+        const verb = new Verb('fly')
+        expect(verb.getPresentSimple(subject)).toBe('flies')
+      })
+
+      it('should return has to irregular verb have', () => {
+        const verb = new Verb('have')
+        expect(verb.getPresentSimple(subject)).toBe('has')
+      })
+
+    })
+
   })
 
 })
--- src/model/verb/verb.ts
+++ src/model/verb/verb.ts
@@ -1,11 +1,34 @@
+import { Subject } from '..'
+
 export class Verb {
 
-  constructor(
-    private value: string,
-  ) { }
+  value: string
 
-  getInfinitive() {
+  constructor(value: string) {
+    this.value = value.toLowerCase()
+  }
+
+  getInfinitive(): string {
     return this.value
+  }
+
+  getPresentSimple(subject: Subject): string {
+    if (subject.isThirdPerson())
+      return this.getPresentSimpleForThirdPerson()
+    return this.value
+  }
+
+  private getPresentSimpleForThirdPerson(): string {
+    if (this.value.match(/(o|s|z|ch|sh|x)$/))
+      return this.value + 'es'
+
+    if (this.value.endsWith('y'))
+      return this.value.slice(0, -1) + 'ies'
+
+    if (this.value === 'have')
+      return 'has'
+
+    return this.value + 's'
   }
 
 }
